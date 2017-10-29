0d38e222331c038227d504e25d0d065c103a4cc8
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2017-10-28 20:29:38 -0700

    group listings

--- src/app/group/grouplisting.component.ts
+++ src/app/group/grouplisting.component.ts
@@ -12,33 +12,33 @@ import { GroupService } from '../database/group.service';
     styleUrls:['grouplisting.component.css']
 })
 export class GroupListing {
-    constructor(private account: AccountService, private router: Router, private group: GroupService){}
-    groups = null;
-    name = "test";
-    users = {};
-    groupService = null;
-    new(){
-        let self = this;
-        self.group.join(self.account.sessionKey(), self.name);
-    }
-    join(name){
-        let self = this;
-        self.group.join(self.account.sessionKey(), name );
-    }
-    ngOnInit(){
-        let self = this;
-        self.group.client().executeWhenConnected(function () {
-            self.account.executeWhenLoggedIn(function () {
-                self.groupService = self.group;
-                self.group.groupListFunction = function(data){
-                    self.groups = Object.keys(data);
-                };
-                self.group.joinFunction = function (data) {
-                    self.router.navigate(['/group']);
-                };
-                self.group.listen(self.account.sessionKey());
-                self.group.listing(self.account.sessionKey());
-            });
-        });
-    }
+  constructor(private account: AccountService, private router: Router, private group: GroupService){}
+  groups = [];
+  name = "test";
+  users = {};
+  groupService = null;
+  new(){
+    let self = this;
+    self.group.join(self.account.sessionKey(), self.name);
+  }
+  join(name){
+    let self = this;
+    self.group.join(self.account.sessionKey(), name );
+  }
+  ngOnInit(){
+    let self = this;
+    self.group.groupListFunction = function(data){
+      self.groups = Object.keys(data);
+    };
+    self.group.joinFunction = function (data) {
+      self.router.navigate(['/group']);
+    };
+    self.group.client().executeWhenConnected(function () {
+      self.account.executeWhenLoggedIn(function () {
+        self.groupService = self.group;
+        self.group.listen(self.account.sessionKey());
+        self.group.listing(self.account.sessionKey());
+      });
+    });
+  }
 }
